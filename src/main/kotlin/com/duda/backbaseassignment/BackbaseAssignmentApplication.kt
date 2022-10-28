8bc952a600cffb8ba5f1ef0fef78cadc79c866b3
Eduardo Meneses <eduardo.demeneses@gmail.com>
2022-10-28 23:41:06 +0200

    initial commit

---
+++ src/main/kotlin/com/duda/backbaseassignment/BackbaseAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package com.duda.backbaseassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackbaseAssignmentApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackbaseAssignmentApplication>(*args)
+}
---
+++ src/test/kotlin/com/duda/backbaseassignment/BackbaseAssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.duda.backbaseassignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackbaseAssignmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
